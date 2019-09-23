8c363f5f555ffa97bbb2bcd858bb28a429da6b29
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-22 22:21:55 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,25 @@ client.on('message', message => {
 	} else if (command === 'beep') {
 		message.channel.send('Boop.');
 	}
-	
+	if (message.content.startsWith(prefix +"generar" )){
+	    const embed = new Discord.RichEmbed() 
+	    .setTitle("Este es su título, puede contener 256 caracteres")
+	    .setAuthor(message.author.username, message.author.avatarURL)
+	    .setColor(0x00AE86)
+	    .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
+	    .setFooter("Pie de página, puede contener 2048 caracteres", client.user.avatarURL)
+	    .setImage(message.author.avatarURL)
+	    .setThumbnail(message.author.avatarURL)
+	    .setTimestamp()
+	    .setURL("https://github.com/CraterMaik")
+	    .addField("Este es un título de campo, puede contener 256 caracteres",
+	      "Este es un valor de campo, puede contener 2048 caracteres.")
+	    .addField("Campo en línea", "Debajo del campo en línea", true)
+	    .addBlankField(true)
+	    .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
+	    
+	    message.channel.send({embed});
+	}
 	if (command === 'ip') {
 	
 	const ipEmbed = new Discord.RichEmbed()
