f37d98f933128de70914994c33148464170a09bd
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 20:23:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,9 @@
-vconst Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = "!"
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
-client.on("ready", () => {
-   console.log(`Estoy listo!, conectado en ${client.guilds.size} servidores y  ${client.users.size} usuarios.`);
-});
+client.on('ready', () => {
+    client.user.setActivity("TV", {type: "WATCHING"})
+})
 
-client.on("guildMemberAdd", (member) => {
-   var canal = client.channels.get('625499285215117343'); 
-   canal.send(`${member.user}, bienvenido al servidor pasala bien.`);   
-});
-
-
-client.on("message", (message) => {
-	if(command === prefix + 'aleatorio'){
-	    var rpts = ["Sí", "No", "¿Por qué?", "Por favor", "Tal vez", "No sé", "Definitivamente?", " ¡Claro! "," Sí "," No "," Por supuesto! "," Por supuesto que no "];
-	    if (!texto) return message.reply(`Escriba una pregunta.`);
-	    message.channel.send(message.member.user+' a su pregunta `'+texto+'` mi respuesta es: `'+ rpts[Math.floor(Math.random() * rpts.length)]+'`');
-
-	}
-
-});
 
 client.login(process.env.BOT_TOKEN);
