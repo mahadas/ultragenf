9b45880024f35392ba5185703d32130292d633a4
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 20:31:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,17 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const prefix = "!"
 
-
-client.on('ready', () => {
-    client.user.setActivity("!ayuda - UltraGen", {type: "PLAYING"})
-
-})
+client.on("ready", () => {
+   console.log(`Estoy listo!, conectado en ${client.guilds.size} servidores y  ${client.users.size} usuarios.`);
+   client.user.setPresence( {
+       status: "online",
+       game: {
+           name: `-help | Estoy en ${client.guilds.size} servidores.`,
+           type: "PLAYING"
+       }
+    });
+});
 
 client.on("message", (message) => {
   if (message.content.startsWith(prefix + "ping")) {
