1d95ad2a0fc06d7e026389f9d55e86650fddcd03
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 20:28:22 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,31 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+
 client.on('ready', () => {
-    client.user.setActivity("TV", {type: "WATCHING"})
+    client.user.setActivity("!ayuda - UltraGen", {type: "PLAYING"})
+
 })
+
+client.on('message', (received message) => {
+	if (receivedMessage.content.startwith("!")) {
+		if (primaryCommand == "ayuda") {
+			if (arguments.length > 0) {
+				receivedMessage.channel.send("Aca estoy para poder ayudarte")
+			} else {
+				receivedMessage.channel.send("!ayuda [comando]")
+			}
+
+
+		}
+
+
+
+	}
+
+
+
+}
 
 
 client.login(process.env.BOT_TOKEN);
