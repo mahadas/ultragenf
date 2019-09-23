3a6c715210cf77c06d25b3093eded6f09f24517f
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 20:38:29 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,30 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "hola")) {
-    message.channel.send("Hola que tal?");
+	if (message.content.startsWith(prefix + "ping")) {
+		message.channel.send("pong!");
   }
+	if (message.content.startsWith(prefix + "hola")) {
+		message.channel.send("Hola que tal?");
+  }
+	if (message.content.startsWith(prefix +"ayuda" )){
+		const embed = new Discord.RichEmbed() 
+	    .setTitle("Este es su título, puede contener 256 caracteres")
+	    .setAuthor(message.author.username, message.author.avatarURL)
+	    .setColor(#ff0000)
+	    .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
+	    .setFooter("Creado por mahada#0641", client.user.avatarURL)
+	    .setImage(message.author.avatarURL)
+	    .setThumbnail(message.author.avatarURL)
+	    .setTimestamp()
+	    .setURL("https://github.com/CraterMaik")
+	    .addField("IP","Ver tu conexion en este servidor.")
+	    .addBlankField(true)
+	    .addField("Generar", "Genera tu cuenta de todo tipo", true)
+	    
+	    message.channel.send({embed});
+	}
+
 });
 
 
