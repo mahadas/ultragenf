61c3c50a1241667d88defe642ef326a3a6b7d62f
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-22 22:23:43 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "!"
 
 client.once('ready', () => {
 	console.log('El bot ha sido iniciado correctamente.');
 });
 
-const prefix = "!"
-
 client.on('message', message => {
-	if (command === 'ping') {
-		message.channel.send('Aca tienes tu ping.');
-	} else if (command === 'beep') {
-		message.channel.send('Boop.');
-	}
 	if (message.content.startsWith(prefix +"generar" )){
 	    const embed = new Discord.RichEmbed() 
 	    .setTitle("Este es su título, puede contener 256 caracteres")
@@ -31,18 +25,6 @@ client.on('message', message => {
 	    .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
 	    
 	    message.channel.send({embed});
-	}
-	if (command === 'ip') {
-	
-	const ipEmbed = new Discord.RichEmbed()
-	.setColor('#0099ff')
-	.setTitle('FmxCraft BOT')
-	.setURL('https://discord.js.org/')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Regular field title', 'Some value here')
-	.addBlankField()
-	
-	channel.send(ipEmbed);
 	}
 });
 
