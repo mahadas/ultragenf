3178cf0b699276adc211b035b8d03cafe976df12
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 19:59:34 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,20 +7,25 @@ client.on("ready", () => {
    client.user.setPresence( {
        status: "online",
        game: {
-           name: `-help | Estoy en ${client.guilds.size} servidores.`,
-           type: "PLAYING"
+           name: `!help | UltraGen`,
+           type: "STREAMING"
        }
     });
+});
+
+client.on("guildMemberAdd", (member) => {
+   var canal = client.channels.get('625499285215117343'); 
+   canal.send(`${member.user}, bienvenido al servidor pasala bien.`);
+   
 });
 
 client.on('message', message => {
 	if (message.content.startsWith(prefix +"generar" )){
 	    const embed = new Discord.RichEmbed() 
 	    .setTitle("Este es su título, puede contener 256 caracteres")
-	    .setAuthor(message.author.username, message.author.avatarURL)
 	    .setColor(0x00AE86)
 	    .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
-	    .setFooter("Pie de página, puede contener 2048 caracteres", client.user.avatarURL)
+	    .setFooter("Version 1.0 para UltraGen - Desarrollado por mahada#0641")
 	    .setImage(message.author.avatarURL)
 	    .setThumbnail(message.author.avatarURL)
 	    .setTimestamp()
@@ -33,6 +38,19 @@ client.on('message', message => {
 	    
 	    message.channel.send({embed});
 	}
+	if (command === prefix +'ping') {
+
+    let ping = Math.floor(message.client.ping);
+    
+    message.channel.send(":ping_pong: Pong!")
+      .then(m => {
+
+          m.edit(`:incoming_envelope: Ping Mensajes: \`${Math.floor(m.createdTimestamp - Date.now())} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
+      
+      });
+    
+  }
+  
 });
 
 client.login(process.env.BOT_TOKEN);
