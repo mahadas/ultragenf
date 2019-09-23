ca18d9f9fdaa508f8239702dc073363da2bc0405
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 20:41:48 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,10 @@ client.on("message", (message) => {
 	if (message.content.startsWith(prefix + "ping")) {
 		message.channel.send("pong!");
 	}	
+	if(command === "ping") {
+		const m = await message.channel.send("Ping?");
+		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+	}	
 });
 
 client.login(process.env.BOT_TOKEN);
