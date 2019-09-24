464fbef5a9b1aef50b5469e8fdc0282d08e13e24
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 21:00:07 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,14 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const prefix = "!" //Bot command prefix
+var request = require('request');
+var CMD = 'ping'; //Command to trigger
+var mcIP = 'mc.heavenmc.es'; //Add your Minecraft server IP
+var mcPort = 25565; //The port of the server, default it 25565
+var serverName = 'HeavenMC NetWork'; //Your server name
+var serverUrl = "https://minecraft.net"; //Server website
+var serverLogo = "https://images-eu.ssl-images-amazon.com/images/I/512dVKB22QL.png"; //Server logo
+
 
 client.on("ready", () => {
    console.log(`Estoy listo!, conectado en ${client.guilds.size} servidores y  ${client.users.size} usuarios.`);
@@ -12,9 +21,59 @@ client.on("ready", () => {
     });
 });
 
-client.on("message", async message => {
-	const prefix = "!"
-	if (message.content.startsWith(prefix + "ping")) {
-		message.channel.sendMessage('Si Internet Es De `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-	}
+
+client.on('message', message => {
+
+  if (message.content === prefix + CMD) {
+    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+    request(url, function (err, response, body) {
+      if (err) {
+        console.log(err);
+        return message.reply('Error getting Minecraft server status...');
+      }
+      
+      body = JSON.parse(body);
+      var status = "Offline"
+      var color = 16711680
+      if (body.online) {
+        status = "Online";
+        color = 65280
+      }
+      var players = 0
+      if (body.players.now) {
+        players += body.players.now;
+      }
+      else {
+        players += 0
+      }
+      
+      const embed = {
+        "author": {
+          "name": serverName + " Server Status",
+          "url": serverUrl,
+          "icon_url": serverLogo
+        },
+        "color": color,
+        "fields": [
+          {
+            "name": "Status:",
+            "value": status,
+            "inline": true
+          },
+          {
+            "name": "Players Online:",
+            "value": "**" + body.players.now + "** / **" + body.players.max + "**",
+            "inline": true
+          }
+        ],
+        "footer": {
+          "text": "IP: " + mcIP
+        }
+      };
+      message.channel.send({ embed });
+    });
+  };
+});
+
+
 client.login(process.env.BOT_TOKEN);
