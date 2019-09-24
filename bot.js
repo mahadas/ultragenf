5b7c7167cd2b80e9480948b3d253b6688aa06d3e
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-23 21:04:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,6 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const prefix = "!" //Bot command prefix
 var request = require('request');
-var CMD = 'ping'; //Command to trigger
-var mcIP = 'mc.heavenmc.es'; //Add your Minecraft server IP
-var mcPort = 25565; //The port of the server, default it 25565
-var serverName = 'HeavenMC NetWork'; //Your server name
-var serverUrl = "https://minecraft.net"; //Server website
-var serverLogo = "https://images-eu.ssl-images-amazon.com/images/I/512dVKB22QL.png"; //Server logo
 
 
 client.on("ready", () => {
@@ -24,19 +18,19 @@ client.on("ready", () => {
 
 client.on('message', message => {
 
-  if (message.content === prefix + CMD) {
-    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+  if (message.content === prefix + "estado") {
+    var url = 'http://mcapi.us/server/status?ip=' + 'mc.heavenmc.es' + '&port=' + '25565';
     request(url, function (err, response, body) {
       if (err) {
         console.log(err);
-        return message.reply('Error getting Minecraft server status...');
+        return message.reply('El servidor no esta conectado correcamente');
       }
       
       body = JSON.parse(body);
-      var status = "Offline"
+      var status = "Inactivo"
       var color = 16711680
       if (body.online) {
-        status = "Online";
+        status = "Activo";
         color = 65280
       }
       var players = 0
@@ -49,25 +43,25 @@ client.on('message', message => {
       
       const embed = {
         "author": {
-          "name": serverName + " Server Status",
-          "url": serverUrl,
-          "icon_url": serverLogo
+          "name": "HeavenMC NetWork" + " Server Status",
+          "url": "tienda.heavenmc.es",
+          "icon_url": "https://cdn.tebex.io/webstore/799123/images/799123-98e9b9ee1e018f32203ec1984acb30a2459c79d9.png"
         },
         "color": color,
         "fields": [
           {
-            "name": "Status:",
+            "name": "Estado:",
             "value": status,
             "inline": true
           },
           {
-            "name": "Players Online:",
+            "name": "Jugadores Conectados:",
             "value": "**" + body.players.now + "** / **" + body.players.max + "**",
             "inline": true
           }
         ],
         "footer": {
-          "text": "IP: " + mcIP
+          "text": "IP: " + "mc.heavenmc.es"
         }
       };
       message.channel.send({ embed });
