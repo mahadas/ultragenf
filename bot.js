9c7aad29bb97f1bc7a788108aefbd7b29d140fbd
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-24 19:24:22 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const prefix = "!"
 
+
 client.on("ready", () => {
    console.log(`Estoy listo!, conectado en ${client.guilds.size} servidores y  ${client.users.size} usuarios.`);
    client.user.setPresence( {
@@ -23,6 +24,16 @@ client.on("message", (message) => {
 	if (message.content.startsWith(prefix + "foo")) {
 		message.channel.send("bar!");
  	 }
+	else if (command === 'args-info') {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		}
+		else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+
+		message.channel.send(`First argument: ${args[0]}`);
+	}
 });
 
 
