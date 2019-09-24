3ecfb59451b46a77bdbb7872c9929ccdd33211dc
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-24 18:52:38 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,63 +15,22 @@ client.on("ready", () => {
 
 
 client.on('message', message => {
-  var request = require('request');
-  var Comando = 'estado';
-  var IP = 'mc.heavenmc.es';
-  var Puerto = 25565;
-  var NombreServer = 'HeavenMC NetWork';
-  var TiendaServer = "https://tienda.heavenmc.es";
-  var LogoServer = "https://cdn.tebex.io/webstore/799123/images/799123-98e9b9ee1e018f32203ec1984acb30a2459c79d9.png";
-  if (message.content === prefix + Comando) {
-    var url = 'http://mcapi.us/server/status?ip=' + IP + '&port=' + Puerto;
-    request(url, function (err, response, body) {
-      if (err) {
-        console.log(err);
-        return message.reply('No se pudo conectar el servidor');
-      }
-      
-      body = JSON.parse(body);
-      var status = "Inactivo"
-      var color = 16711680
-      if (body.online) {
-        status = "Activo";
-        color = 65280
-      }
-      var players = 0
-      if (body.players.now) {
-        players += body.players.now;
-      }
-      else {
-        players += 0
-      }
-      
-      const embed = {
-        "author": {
-          "name": (body.motd),
-          "url": TiendaServer,
-          "icon_url": LogoServer
-        },
-        "color": color,
-        "fields": [
-          {
-            "name": "Estado:",
-            "value": status,
-            "inline": true
-          },
-          {
-            "name": "Jugadores Conectados:",
-            "value": "**" + body.players.now + "** / **" + body.players.max + "**",
-            "inline": true
-          }
-        ],
-        "footer": {
-          "text": "IP: " + IP
-        }
-      };
-      message.channel.send({ embed });
-    });
-  };
+client.on('message', message => {
+	var Comando = 'anuncio';
+	if (message.content === prefix + Comando) {
+		if (args[0]) {
+			const user = getUserFromMention(args[0]);
+			if (!user) {
+				return message.reply('Please use a proper mention if you want to see someone else\'s avatar.');
+			}
+
+			return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL}`);
+		}
+
+		return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL}`);
+	}  	
 });
+
 
 
 client.login(process.env.BOT_TOKEN);
