1651ae030174c6b9c0aa2cbc062e3fe28da26023
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-24 20:11:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,19 +5,23 @@ const prefix = "!"
 
 client.on("ready", () => {
    console.log(`Estoy listo!, conectado en ${client.guilds.size} servidores y  ${client.users.size} usuarios.`);
-   client.user.setPresence( {
-       status: "online",
-       game: {
-           name: `-help | Estoy en ${client.guilds.size} servidores.`,
-           type: "PLAYING"
-       }
-    });
+client.user.setPresence({
+	       status: "online",
+	       game: {
+	           name: "-help | crater en directo",
+	           url: "https://www.twitch.tv/CraterMaik",
+	           type: "STREAMING"
+	       }
+	   });
 });
 
 
 client.on("message", (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
  	const command = args.shift().toLowerCase();	
+	if (message.content.startsWith(prefix + "ping")) {
+		message.channel.send("pong!");
+	} else
 	if (message.content.startsWith(prefix + "foo")) {
 		message.channel.send("bar!");
  	 }
@@ -31,11 +35,39 @@ client.on("message", (message) => {
 
 		message.channel.send(`First argument: ${args[0]}`);
 	}
-	if (message.content.startsWith(prefix + "anuncio")) {
+	else if (command === 'anuncio') {
 		let text = args.join(" ");
 		message.delete();
-		message.channel.send(text);
+		message
+		message.channel.send({embed: {
+			color: 3447003,
+			description: '(text)'
+		}});
+
 	}
+ if (command === "ban") {
+   let buser = message.mentions.users.first();
+   let breason = args.join(" ");
+   if (!buser) return message.channel.send("Menciona a alguien")
+   if (!breason) return message.channel.send("Dime la razon del ban")
+   if (!message.member.hasPermission["BAN_MEMBERS"]) return message.channel.send("Sin permisos bro")
+   
+  const bembed = new Discord.RichEmbed()
+   .setAuthor('Baneos de HeavenMC Discord')
+   .setThumbnail(buser.avatarURL)
+   .setColor("0xFF0000")
+   .addField("User Baneado", buser.username)
+   .addField("ID", buser.id)
+   .addField("Motivo", breason)
+   .addField("Staff", message.author.username)
+   message.guild.member(buser).ban(breason).reason
+   buser.send(bembed)
+   message.channel.send(bembed)
+   
+   }
+ 
+
+	
 });
 
 
