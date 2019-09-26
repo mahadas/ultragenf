1662541f1494a4348d3521ec19b5ff5e664dd440
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-25 21:39:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,17 +36,15 @@ client.on("message", (message) => {
 
 		}
 	}
-    if (message.content.startsWith == prefix + "clear") {
-        try {
-            if (message.member.hasPermission("MANAGE_MESSAGES")) {
-                messages = message.channel.fetchMessages();
-                message.channel.bulkDelete(messages);
-            }
-        } catch(e) {
-            message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
-            console.log(e);
+	if (message.content == "clear") {
+		if (message.member.hasPermission("MANAGE_MESSAGES")) {
+		message.channel.fetchMessages()
+ 		.then(function(list){
+			message.channel.bulkDelete(list);
+			}, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
         }
-    }	
+    }
+
 
 
 });
