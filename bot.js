d0ad861c49d1071f6488ec39e482ce46cf3374bb
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-25 21:37:32 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,13 +17,37 @@ client.on("ready", () => {
 client.on("message", (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();	
-	if (command === 'ping') {
+	if (command === 'conexion') {
 		let ping = Math.floor(message.client.ping);
 		message.channel.send({embed: {
 				color: 7419530,
 				description: `:satellite_orbital: Tu conexion es de ${ping} ms`
 			}});	
 	}
+	if (message.content.startsWith(prefix + "prueba")) {
+		if (message.channel.id === '626559477566406666') {	
+			message.channel.send('estas en este canal')
+		}
+		else {
+			message.channel.send({embed: {
+				color: 15158332,
+				description: 'No puedes usar este comando en este canal.'
+			}});			
+
+		}
+	}
+    if (message.content.startsWith == prefix + "clear") {
+        try {
+            if (message.member.hasPermission("MANAGE_MESSAGES")) {
+                messages = message.channel.fetchMessages();
+                message.channel.bulkDelete(messages);
+            }
+        } catch(e) {
+            message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
+            console.log(e);
+        }
+    }	
+
 
 });
 
