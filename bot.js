081caae11dffe838b29e79e4f84cc9488717ab77
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-25 21:59:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on("message", (message) => {
 			if (args[0] === 'minecraft') {
 				var minecrafts = ["Your facts", "...", "..."];
 				var mine = Math.floor(Math.random() * minecrafts.length);
-				message.author.sendMessage(minecrafts[mine])
+				message.author.sendMessage(minecrafts[mine]) return
 				message.channel.send({embed: {
 					color: 1752220,
 					description: 'La cuenta se te envio al mensaje privado.'
