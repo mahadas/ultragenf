ccc3e7616a03625ba6ecafc095d37504efab01ca
mahadas <54777320+mahadas@users.noreply.github.com>
2020-11-01 20:34:15 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,17 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const prefix = "!"
+const prefix = "$"
 
 client.on("ready", () => {
 	client.user.setPresence({
        status: "online",
        game: {
-           name: "!ayuda | UltraGen",
-           url: "https://youtube.com/c/FacuJM",
+           name: "$ayuda | Verificación Unica",
+           url: "https://twitter.com/_mahada_",
            type: "STREAMING"
        }
    });
 });
-
 
 client.on("message", (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -24,28 +23,6 @@ client.on("message", (message) => {
 				description: `:satellite_orbital: Tu conexion es de ${ping} ms`
 			}});	
 	}
-	if (message.content.startsWith(prefix + "generar")) {
-		if (message.channel.id === '626559477566406666') {	
-			if (args[0] === 'minecraft') {
-				var minecrafts = ["Your facts", "...", "..."];
-				var mine = Math.floor(Math.random() * minecrafts.length);
-				message.author.sendMessage(minecrafts[mine]) return
-				message.channel.send({embed: {
-					color: 1752220,
-					description: 'La cuenta se te envio al mensaje privado.'
-				}});	
-			}
-		}		
-		else {
-			message.channel.send({embed: {
-				color: 15158332,
-				description: 'No puedes usar este comando en este canal.'
-			}});			
-
-		}
-	}
-
-
 });
 
 
