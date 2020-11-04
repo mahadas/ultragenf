ef74f6c950b71a8383562a0414c4e4c2bc517031
mahadas <54777320+mahadas@users.noreply.github.com>
2020-11-04 00:07:32 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,6 +50,19 @@ client.on("message", (message) => {
 		message.channel.send('El usuario es ' + message.author.tag);
 	
 	}
+	if (command === 'dado') {
+		let limit = args[0];
+		if (!limit) limit = 6;
+   		const n = Math.floor(Math.random() * limit + 1);
+		 if (!n || limit <= 0)
+		      return this.sendErrorMessage(message, 0, 'Please provide a valid number of dice sides');
+		    const embed = new MessageEmbed()
+		      .setTitle('🎲  Dice Roll  🎲')
+		      .setDescription(`${message.member}, you rolled a **${n}**!`)
+		      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+		      .setTimestamp()
+		      .setColor(message.guild.me.displayHexColor);
+		    message.channel.send(embed);
 });
 
 
